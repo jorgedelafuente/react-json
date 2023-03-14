d4a812c785966dba7065bd1a5279673f798bd3b3
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2023-03-14 15:34:22 +0100

    feat : toasts

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import { PostContainer } from './features/posts';
+import 'react-toastify/dist/ReactToastify.css';
 import './globals.scss';
 
 const root = ReactDOM.createRoot(
@@ -13,6 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <PostContainer />
     </Provider>
   </React.StrictMode>
